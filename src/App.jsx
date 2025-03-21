503949a25b2685f883937c10cf57ad6489fc0fa4
DariaAleshina <ddaleshina@gmail.com>
2025-03-21 13:41:02 +0100

    header: changed img loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDesriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function generateRandomInt(maxNumber) {
@@ -5,13 +7,16 @@ function generateRandomInt(maxNumber) {
 }
 
 function Header() {
+  const randomDescription =
+    reactDesriptions[generateRandomInt(reactDesriptions.length - 1)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDesriptions[generateRandomInt(reactDesriptions.length - 1)]} React
-        concepts you will need for almost any app you are going to build!
+        {randomDescription} React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
   );
