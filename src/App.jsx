1ef6b4f76565b4feb6b7bc1c47c711bb04d3dc18
DariaAleshina <ddaleshina@gmail.com>
2025-03-25 14:29:56 +0100

    tab content added dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/Example/TabButton.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
   const handleClick = function (selectedBtn) {
     console.log(`clicked button: ${selectedBtn}`);
+    setSelectedTopic(selectedBtn);
+    console.log('selectedTopic: ', selectedTopic);
   };
 
   return (
@@ -31,7 +37,13 @@ function App() {
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
-          Dynamic content
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </search>
       </main>
     </div>
